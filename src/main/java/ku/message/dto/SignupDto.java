b9677bc3d21459d832a46f8e669d90c8c750faf3
jade <bleachjade@hotmail.com>
2021-03-25 11:13:24 +0700

    add email validation

--- src/main/java/ku/message/dto/SignupDto.java
+++ src/main/java/ku/message/dto/SignupDto.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
@@ -18,10 +19,16 @@ public class SignupDto {
     private String password;
 
     @NotBlank(message = "First name is required")
+    @Pattern(regexp = "^[a-zA-Z]+$",
+            message = "First name can only contain letters")
     private String firstName;
 
     @NotBlank
     private String lastName;
+
+    @Email
+    @NotBlank
+    private String email;
 
     private String role;
 }
