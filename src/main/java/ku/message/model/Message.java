aff77f279e7a4fd6406bffac94b655b628587427
jade <bleachjade@hotmail.com>
2021-03-25 09:49:36 +0700

    add Lombok

---
+++ src/main/java/ku/message/model/Message.java
@@ -0,0 +1,23 @@
+package ku.message.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String user;
+    private String text;
+
+}
---
+++ src/main/java/ku/message/model/User.java
@@ -0,0 +1,28 @@
+package ku.message.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+    private String password;
+
+    private String firstName;
+    private String lastName;
+
+    private String role;
+
+}
