95e9542938ca30d2ff0b0a178875c11c37f63ab7
jade <bleachjade@hotmail.com>
2021-03-25 10:00:34 +0700

    use UUID instead of normal id

--- src/main/java/ku/message/model/User.java
+++ src/main/java/ku/message/model/User.java
@@ -2,11 +2,8 @@ package ku.message.model;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
@@ -14,15 +11,13 @@ import javax.persistence.Id;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @GeneratedValue(generator = "UUID")
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
 
     private String username;
     private String password;
-
     private String firstName;
     private String lastName;
-
     private String role;
-
 }
